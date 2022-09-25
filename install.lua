0c375836eebef3d39755a596be55c5cca77495cd
matt78229 <114359641+matt78229@users.noreply.github.com>
2022-09-26 04:57:56 +1000

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,8 +2,8 @@
 --
 --
 
-local libURL = "https://raw.githubusercontent.com/acidjazz/drmon/master/lib/f.lua"
-local startupURL = "https://raw.githubusercontent.com/acidjazz/drmon/master/drmon.lua"
+local libURL = "https://raw.githubusercontent.com/matt78229/drmon/master/lib/f.lua"
+local startupURL = "https://raw.githubusercontent.com/matt78229/drmon/master/drmon.lua"
 local lib, startup
 local libFile, startupFile
 
